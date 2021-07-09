a287eb65d1fd46fd5e3e0024b960e8cf91a95a74
Peter Knall <peterknall@gmail.com>
2021-07-08 21:15:13 -0400

    Initial commit

---
+++ src/main/java/com/github/springinaction5examples/SpringInAction5ExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.springinaction5examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInAction5ExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInAction5ExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springinaction5examples/SpringInAction5ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springinaction5examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInAction5ExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
