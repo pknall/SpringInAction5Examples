b581670dae28018b7f1b8dbc75edad41e8f5961f
Peter Knall <peterknall@gmail.com>
2021-07-08 21:21:08 -0400

    Completed Chapter 1 Example.

---
+++ CH01/src/test/java/com/example/ch01/Ch01ApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.ch01;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+// Changed based on https://www.baeldung.com/junit-5-runwith
+// @RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)   // <1>
+public class Ch01ApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;   // <2>
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))    // <3>
+
+                .andExpect(status().isOk())  // <4>
+
+                .andExpect(view().name("home"))  // <5>
+
+                .andExpect(content().string(           // <6>
+                        containsString("Welcome to...")));
+    }
+
+}
